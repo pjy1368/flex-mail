b9a38a56ae2988a23f97c2ff6527a4cfb13bb5ef
Jayon <basejin0810@naver.com>
2025-08-15 15:16:32 +0900

    Initial commit

---
+++ src/main/java/com/jayon/flexmail/FlexMailApplication.java
@@ -0,0 +1,13 @@
+package com.jayon.flexmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexMailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlexMailApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jayon/flexmail/FlexMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayon.flexmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexMailApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
